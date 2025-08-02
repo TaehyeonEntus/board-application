e2392baf8ad4cdbffb58d9026199a24df045b17c
KTH <taehyeonentus@gmail.com>
2025-08-02 18:04:03 +0900

    Add CommentTest Case

--- resourceServer/src/test/java/entus/resourceServer/domain/CommentTest.java
+++ resourceServer/src/test/java/entus/resourceServer/domain/CommentTest.java
@@ -1,14 +1,20 @@
 package entus.resourceServer.domain;
 
+import entus.resourceServer.repository.CommentRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 @DataJpaTest
 class CommentTest {
-
+    @Autowired
+    CommentRepository commentRepository;
     @BeforeEach
     void setUp() {
     }
@@ -21,22 +27,35 @@ class CommentTest {
     @DisplayName("AddComment")
     void AddComment() throws Exception {
         //given
+        String reply = "reply";
+        User user = User.createUser(1L);
+        Post post = Post.createPost(user,"Title","Content");
 
         //when
+        Comment comment1 = Comment.addComment(reply,user,post);
+        Comment comment2 = Comment.addComment(reply,user,post);
 
         //then
-
+        assertNotSame(comment1, comment2);
     }
 
     @Test
     @DisplayName("GetId")
     void GetId() throws Exception {
         //given
+        String reply = "reply";
+        User user = User.createUser(1L);
+        Post post = Post.createPost(user,"Title","Content");
 
         //when
+        Comment comment1 = Comment.addComment(reply,user,post);
+        Comment comment2 = Comment.addComment(reply,user,post);
+
+        commentRepository.save(comment1);
+        commentRepository.save(comment2);
 
         //then
-
+        assertNotEquals(comment1.getId(), comment2.getId());
     }
 
     @Test

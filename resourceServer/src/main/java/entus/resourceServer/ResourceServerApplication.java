1173ea509ab16521431c380b8d7693cb615a6bb7
KTH <taehyeonentus@gmail.com>
2025-08-06 22:44:50 +0900

    enable spring caching

--- resourceServer/src/main/java/entus/resourceServer/ResourceServerApplication.java
+++ resourceServer/src/main/java/entus/resourceServer/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package entus.resourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ResourceServerApplication {
 

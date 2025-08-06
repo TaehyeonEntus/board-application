13ad547f3ce51867210a6bec84e17df28dcc7eaa
KTH <taehyeonentus@gmail.com>
2025-08-06 22:40:55 +0900

    add method for front page

--- resourceServer/src/main/java/entus/resourceServer/repository/CommentRepository.java
+++ resourceServer/src/main/java/entus/resourceServer/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package entus.resourceServer.repository;
 import entus.resourceServer.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    public List<Comment> getCommentByOrderByLikeDesc();
 }
--- resourceServer/src/main/java/entus/resourceServer/repository/PostRepository.java
+++ resourceServer/src/main/java/entus/resourceServer/repository/PostRepository.java
@@ -3,5 +3,8 @@ package entus.resourceServer.repository;
 import entus.resourceServer.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    public List<Post> findTop5ByOrderByViewDesc();
 }

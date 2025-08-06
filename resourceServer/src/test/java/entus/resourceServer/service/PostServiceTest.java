7fe6a5e2290b2e9e3aaf31e95ca8e0d83c5282ea
KTH <taehyeonentus@gmail.com>
2025-08-06 22:40:21 +0900

    add test template

---
+++ resourceServer/src/test/java/entus/resourceServer/service/PostServiceTest.java
@@ -0,0 +1,65 @@
+package entus.resourceServer.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Add")
+    void Add() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("Get")
+    void Get() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("GetAll")
+    void GetAll() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("GetTop5Posts")
+    void GetTop5Posts() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+}

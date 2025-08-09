30947375cdec59fabded905f08a3654b3a2b1c35
KTH <taehyeonentus@gmail.com>
2025-08-09 19:30:33 +0900

    add logic which send accessToken/refreshToken request

---
+++ resourceServer/src/main/resources/templates/js/auth.js
@@ -0,0 +1,55 @@
+async function loadProtectedData(endpoint) {
+    try {
+        const res = await fetch(endpoint, {
+            method: "GET",
+            credentials: "include"
+        });
+
+        //access 실패
+        if (res.status === 401) {
+            const errorData = await res.json();
+
+            if (errorData.error === "access_token_error") {
+                const refreshRes = await fetch("/token/refresh", {
+                    method: "POST",
+                    credentials: "include"
+                });
+                //refresh 성공
+                if (refreshRes.ok) {
+                    const retryRes = await fetch(endpoint, {
+                        method: "GET",
+                        credentials: "include"
+                    });
+                    //access 재시도 성공
+                    if (retryRes.ok) {
+                        const data = await retryRes.text();
+                        console.log("Data loaded:", data);
+                    }
+                    //access 재시도 실패
+                    else {
+                        window.location.href = "http://localhost:9000/login";
+                    }
+                }
+                //refresh 실패
+                else {
+                    window.location.href = "http://localhost:9000/login";
+                }
+
+            } else if (errorData.error === "refresh_token_error") {
+                window.location.href = "http://localhost:9000/login";
+            } else {
+                console.error("Unauthorized access");
+            }
+
+        }
+        //access 성공
+        else if (res.ok) {
+            const data = await res.text();
+            console.log("Data loaded:", data);
+        } else {
+            console.error(`Request failed with status: ${res.status}`);
+        }
+    } catch (error) {
+        console.error("Error during request:", error);
+    }
+}

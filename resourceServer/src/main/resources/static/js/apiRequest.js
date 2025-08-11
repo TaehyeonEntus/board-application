c015b8af298f64a56f9485ffb788bc61236ec93d
KTH <taehyeonentus@gmail.com>
2025-08-11 20:32:19 +0900

    change to JS rendering

--- resourceServer/src/main/resources/static/js/apiRequest.js
+++ resourceServer/src/main/resources/static/js/apiRequest.js
@@ -1,4 +1,4 @@
-async function loadProtectedData(endpoint) {
+export async function loadProtectedData(endpoint) {
     const AUTH_SERVER_URL = "http://localhost:9000"
     try {
         const res = await fetch(endpoint, {
@@ -6,7 +6,6 @@ async function loadProtectedData(endpoint) {
             credentials: "include"
         });
 
-        //access 실패
         if (res.status === 401) {
             const errorData = await res.json();
 
@@ -15,42 +14,38 @@ async function loadProtectedData(endpoint) {
                     method: "POST",
                     credentials: "include"
                 });
-                //refresh 성공
+
                 if (refreshRes.ok) {
                     const retryRes = await fetch(endpoint, {
                         method: "GET",
                         credentials: "include"
                     });
-                    //access 재시도 성공
-                    if (retryRes.ok) {
-                        const data = await retryRes.text();
-                        console.log("Data loaded:", data);
-                    }
-                    //access 재시도 실패
-                    else {
-                        window.location.href = AUTH_SERVER_URL + "/login";
-                    }
-                }
-                //refresh 실패
-                else {
-                    window.location.href = AUTH_SERVER_URL + "/login";
-                }
 
+                    if (retryRes.ok) {
+                        return await retryRes.json();
+                    } else {
+                        window.location.href = AUTH_SERVER_URL + "/login";
+                        return null;
+                    }
+                } else {
+                    window.location.href = AUTH_SERVER_URL + "/login";
+                    return null;
+                }
             } else if (errorData.error === "refresh_token_error") {
                 window.location.href = AUTH_SERVER_URL + "/login";
+                return null;
             } else {
                 console.error("Unauthorized access");
+                window.location.href = AUTH_SERVER_URL + "/login";
+                return null;
             }
-
-        }
-        //access 성공
-        else if (res.ok) {
-            const data = await res.text();
-            console.log("Data loaded:", data);
+        } else if (res.ok) {
+            return await res.json();
         } else {
-            console.error(`Request failed with status: ${res.status}`);
+            return null;
         }
     } catch (error) {
-        console.error("Error during request:", error);
+        window.location.href = AUTH_SERVER_URL + "/login";
+        return null;
     }
 }

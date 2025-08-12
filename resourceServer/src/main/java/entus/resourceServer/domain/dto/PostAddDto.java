f5c97c67c0d7423286e0ee7bd2aa95476b9a83ef
KTH <taehyeonentus@gmail.com>
2025-08-12 23:23:16 +0900

    add post validation during adding posts

--- resourceServer/src/main/java/entus/resourceServer/domain/dto/PostAddDto.java
+++ resourceServer/src/main/java/entus/resourceServer/domain/dto/PostAddDto.java
@@ -1,9 +1,12 @@
 package entus.resourceServer.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class PostAddDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
 }

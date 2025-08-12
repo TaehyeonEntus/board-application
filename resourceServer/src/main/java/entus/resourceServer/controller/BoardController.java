93c98b142fef1a5b58d240c78c848ee0b2f5ad27
KTH <taehyeonentus@gmail.com>
2025-08-12 23:24:47 +0900

    update post add controller

--- resourceServer/src/main/java/entus/resourceServer/controller/BoardController.java
+++ resourceServer/src/main/java/entus/resourceServer/controller/BoardController.java
@@ -8,16 +8,21 @@ import entus.resourceServer.domain.dto.PostDto;
 import entus.resourceServer.domain.dto.PostListDto;
 import entus.resourceServer.service.PostService;
 import entus.resourceServer.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,8 +38,8 @@ public class BoardController {
     @ResponseBody
     @GetMapping("/api/board")
     public BoardResponseDto apiBoard(@RequestParam(defaultValue = "0") int page) {
+        Page<Post> postsList = postService.getPostsByPage(PageRequest.of(page, 20, Sort.by("id").descending()));
         List<Post> hotPostsList = postService.getTop5Posts();
-        Page<Post> postsList = postService.getAll(PageRequest.of(page, 20, Sort.by("id").descending()));
 
         List<PostListDto> hotPosts = hotPostsList.stream()
                 .map(PostListDto::new)
@@ -47,27 +52,55 @@ public class BoardController {
         return new BoardResponseDto(hotPosts, posts);
     }
 
+
+
     @GetMapping("/board/{postId}")
-    public String board(@PathVariable Long postId, Model model) {
-        Post post = postService.get(postId);
-
-        PostDto postDto = new PostDto(post);
-
-        model.addAttribute("post", postDto);
+    public String viewPost(@PathVariable String postId) {
         return "viewPost";
     }
 
+    @ResponseBody
+    @GetMapping("/api/board/{postId}")
+    public PostDto apiBoard(@PathVariable Long postId) {
+        return new PostDto(postService.get(postId));
+    }
+
     @GetMapping("/board/add")
-    public String showAddPost() {
+    public String showAddPost(Model model) {
+        System.out.println("model attributes: " + model.asMap());
+
+        if(!model.containsAttribute("postAddDto")) {
+            model.addAttribute("postAddDto", new PostAddDto());
+        }
         return "addPost";
     }
 
-    @PostMapping("/board/add")
-    public String addPost(@ModelAttribute PostAddDto dto, HttpServletRequest request) {
-        Long userId = Long.parseLong(request.getAttribute("userId").toString());
-        User user = userService.get(userId);
+    @GetMapping("/api/board/add")
+    @ResponseBody
+    public Map<String, String> apiBoardAdd() {
+        Map<String, String> body = new HashMap<>();
+        body.put("status", "ok");
+        return body;  // 자동으로 JSON 변환됨
+    }
 
-        Long postId = postService.add(Post.createPost(user, dto.getTitle(), dto.getContent()));
+    //thymeleaf가 valid하기 편해서... 여기는 thymeleaf rendering
+    @PostMapping("/board/add")
+    public String addPost(
+            @Valid @ModelAttribute PostAddDto postAddDto,
+            BindingResult bindingResult,
+            Principal principal,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                System.out.println(error);
+            }
+            model.addAttribute("postAddDto", postAddDto);
+            return "addPost";
+        }
+
+        User user = userService.get(Long.parseLong(principal.getName()));
+        Long postId = postService.add(Post.createPost(user, postAddDto.getTitle(), postAddDto.getContent()));
 
         return "redirect:/board/" + postId;
     }

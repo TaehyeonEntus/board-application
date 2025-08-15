343b2ca8f38edc705a581a9ca66c2c82214b48e0
KTH <taehyeonentus@gmail.com>
2025-08-15 20:55:10 +0900

    add like logic

--- resourceServer/src/main/java/entus/resourceServer/service/CommentService.java
+++ resourceServer/src/main/java/entus/resourceServer/service/CommentService.java
@@ -1,19 +1,37 @@
 package entus.resourceServer.service;
 
-import entus.resourceServer.domain.Comment;
+import entus.resourceServer.domain.*;
+import entus.resourceServer.repository.CommentLikeRepository;
 import entus.resourceServer.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
+    private final UserService userService;
     public Long add(Comment comment) {
         return commentRepository.save(comment).getId();
     }
 
     public Comment get(Long commentId) {
         return commentRepository.findById(commentId).orElseThrow();
+    }
+
+    public Comment like(Long commentId, Long userId) {
+        Comment comment = get(commentId);
+        User user = userService.get(userId);
+        Optional<CommentLike> oCommentLike = commentLikeRepository.findByUserAndComment(user, comment);
+
+        if(oCommentLike.isPresent())
+            commentLikeRepository.delete(oCommentLike.get());
+        else
+            commentLikeRepository.save(new CommentLike(user, comment));
+
+        return comment;
     }
 }

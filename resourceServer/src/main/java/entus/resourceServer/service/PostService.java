a56758167424d08d7151e707670f7194c582fa43
KTH <taehyeonentus@gmail.com>
2025-08-15 20:54:54 +0900

    add viewcount, like logic

--- resourceServer/src/main/java/entus/resourceServer/service/PostService.java
+++ resourceServer/src/main/java/entus/resourceServer/service/PostService.java
@@ -1,19 +1,25 @@
 package entus.resourceServer.service;
 
 import entus.resourceServer.domain.Post;
+import entus.resourceServer.domain.PostLike;
+import entus.resourceServer.domain.User;
+import entus.resourceServer.repository.PostLikeRepository;
 import entus.resourceServer.repository.PostRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final UserService userService;
 
     public Long add(Post post) {
         return postRepository.save(post).getId();
@@ -28,6 +34,24 @@ public class PostService {
     }
 
     public List<Post> getTop5Posts(){
-        return postRepository.findTop5PostsByOrderByIdDesc();
+        return postRepository.findTop5PostsByOrderByViewCountDesc();
+    }
+
+    @Transactional
+    public void addViewCount(Long postId) {
+        postRepository.findById(postId).orElseThrow().addViewCount();
+    }
+
+    public Post like(Long postId, Long userId) {
+        Post post = get(postId);
+        User user = userService.get(userId);
+        Optional<PostLike> oPostLike = postLikeRepository.findByUserAndPost(user, post);
+
+        if(oPostLike.isPresent())
+            postLikeRepository.delete(oPostLike.get());
+        else
+            postLikeRepository.save(new PostLike(user, post));
+
+        return post;
     }
 }

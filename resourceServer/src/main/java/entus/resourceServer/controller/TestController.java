36ad1356c1d24514a4a9338dfc9e067221a4999b
KTH <taehyeonentus@gmail.com>
2025-09-09 23:22:16 +0900

    update test controller

--- resourceServer/src/main/java/entus/resourceServer/controller/TestController.java
+++ resourceServer/src/main/java/entus/resourceServer/controller/TestController.java
@@ -1,22 +1,46 @@
 package entus.resourceServer.controller;
 
+import entus.resourceServer.domain.Post;
+import entus.resourceServer.domain.User;
+import entus.resourceServer.service.PostService;
+import entus.resourceServer.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
 public class TestController {
+    private final PostService postService;
+    private final UserService userService;
+
     @GetMapping("/public")
+    @ResponseBody
     public String publicTest(){
         return "public";
     }
 
     @GetMapping("/user")
+    @ResponseBody
     public String userTest(){
         return "user";
     }
 
     @GetMapping("/admin")
+    @ResponseBody
     public String adminTest() {
         return "admin";
+    }
+
+    @GetMapping("/addTestData")
+    public String addTestData(Principal principal){
+        Long userId = Long.parseLong(principal.getName());
+        System.out.println(userId);
+        User user = userService.get(userId);
+        postService.add(Post.createPost(user,"title","content"));
+        return "redirect:/board";
     }
 }
